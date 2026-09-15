63356d2a979ceda9a6f075f1115d7dcc2e354e4c
valeria <valeriapikul@yandex.ru>
2026-09-15 19:01:18 +0300

    correct components

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 const UserCard = ({ name, age, isOnline }) => {
+  const [status, setStatus] = useState(isOnline);
+
+  let category;
   let text;
 
-  if (isOnline === true) {
+  if (status) {
     text = "В сети";
   } else {
     text = "Не в сети";
   }
 
+  if (age < 18) {
+    category = "Ребёнок";
+  } else if (age >= 18 && age <= 65) {
+    category = "Взрослый";
+  } else {
+    category = "Пенсионер";
+  }
+
   return (
     <div>
       <p>
-        {name} {age} {text}
+        {name} {age} {category} {text}
       </p>
+      <button onClick={() => setStatus(!status)}>Сменить статус</button>
     </div>
   );
 };
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,17 +1,25 @@
 import UserCard from "./UserCard";
 
-const UserList = ({ users }) => {
+const showList = (users) => {
   return (
     <ul>
       {users.map((user) => {
         return (
           <li key={user.id}>
-            <UserCard name={user.name} age={user.age} isOnline={user.isOnline} />
+            <UserCard
+              name={user.name}
+              age={user.age}
+              isOnline={user.isOnline}
+            />
           </li>
         );
       })}
     </ul>
   );
+};
+
+const UserList = ({ users }) => {
+  return users.length === 0 ? "Пользователей нет" : showList(users);
 };
 
 export default UserList;
